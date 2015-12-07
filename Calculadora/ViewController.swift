e805ca459fae79d2fe77f879ca855db70a50ca50
Aderlei Filho <aderleifilho@gmail.com>
2015-12-07 16:50:29 -0200

    Lecture 2

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
     // @IBOutlet is not part of Swift syntax
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    // Everytime we initialize a variable we don't need to specify its type
+    // Let Swift infer from the initialized value instead
+    // var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
+    var operandStack = Array<Double>()
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 
     @IBAction func appendDigit(sender: UIButton) {
         
@@ -27,5 +40,42 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func enter() {
+        
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+        
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×" : performOperation { $0 * $1 }
+        case "÷" : performOperation { $1 / $0 }
+        case "+" : performOperation { $0 + $1 }
+        case "−" : performOperation { $1 - $0 }
+        case "√" : performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
 }
 
